4e256996efce403d9887368363675193157bb404
lamgaopc <hophamlam92@yahoo.com>
2025-04-06 01:41:07 +0700

    go

--- netlify/functions/supabase.js
+++
@@ -1,7 +0,0 @@
-// new code
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
